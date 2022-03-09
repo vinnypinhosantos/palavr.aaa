96105ce9392bd0c8254de73215661ece3875d046
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-03-08 21:31:22 -0300

    Primeiro esboço
    Primeiro esboço de código tem uma lista pequena de palavras que o usuário pode "chutar" por um número de tentativas pré-definido. Ele percorre o palpite e se o usuário acertou a letra no lugar correto fica verde, se tem a letra mas não está no lugar correto fica amarelo.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import random
 
+tentativas = 5
+
+palavras = ["loira", "manta", "fraco", "pouco", "trigo", "certo", "venho"]
+palavra_escolhida = random.choice(palavras)
+
+palpite = input("Digite seu palpite: ")
+tentativa_atual = 1
+
+while tentativa_atual < tentativas:
+    letra_atual = 0
+    for letra in palpite:
+        if letra == palavra_escolhida[letra_atual]:
+            print(f"\033[32m{letra.upper()}\033[0;0m", end=" ")
+        else:
+            if letra in palavra_escolhida:
+                print(f"\033[33m{letra.upper()}\033[0;0m", end=" ")
+            else:
+                print(f"{letra.upper()}", end=" ")
+        letra_atual += 1
+    if palpite == palavra_escolhida:
+        print("\nVocê acetou!")
+        break
+    tentativa_atual += 1
+    palpite = input("\nDigite um novo palpite: ")
+    if palpite == palavra_escolhida:
+        print("\nVocê acetou!")
+        break
+
+if palpite != palavra_escolhida:
+    print(f"VOCÊ ERROU! A PALAVRA ERA {palavra_escolhida.upper()}")
